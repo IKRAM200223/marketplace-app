62973a6df03351860acf5ef55344f82166307945
IKRAM200223 <aitikram642@gmail.com>
2025-06-17 21:57:11 +0100

    SRC/index.js
    import React from 'react';
    import reactDOM from 'react-dom/client';
    import App from './App';
    const root = ReactDOM.createROOT(document.getelementbyId('root'));
    root.render(<App/>);

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import reactDOM from 'react-dom/client';
+import App from './App';
+const root = ReactDOM.createROOT(document.getelementbyId('root'));
+root.render(<App/>);
